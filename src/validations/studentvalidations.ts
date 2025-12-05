22922fd796af869fd179acffe2983933e56b3db1
pavan <pavantirunagar11514@gmail.com>
2025-12-05 13:15:04 +0530

    validation

--- src/validations/studentvalidations.ts
+++ src/validations/studentvalidations.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const studentRegisterSchema = z.object({
-  name: z.string().min(3, "Name is required"),
-  email: z.string().email("Invalid email"),
-  phone: z.string().min(10, "Phone number must be at least 10 digits"),
-  room: z.string().min(1, "Room ID is required"),
-  address: z.string().min(3, "Address is required")
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Valid email is required"),
+  phone: z.string().min(10, "Phone number required"),
+  address: z.string().min(1, "Address is required"),
+  room: z.string().min(1, "Room is required"),
+  joinDate: z.string().min(1, "Join Date is required"),
 });
